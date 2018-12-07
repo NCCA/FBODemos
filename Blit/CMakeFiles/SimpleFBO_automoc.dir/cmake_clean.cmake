8beb6ad22ce6530fad83de1a218b8654802c0608
jmacey <jmacey@bournemouth.ac.uk>
2018-12-07 16:18:02 +0000

    removed all the CMakeFiles stuff

--- Blit/CMakeFiles/SimpleFBO_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "SimpleFBO_automoc.cpp"
-  "CMakeFiles/SimpleFBO_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SimpleFBO_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
